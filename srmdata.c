rclasen/srmio

commit 2da86c0ecce19625e8808e929d8b422aa2cb4871

moved data fixup from download to srmdata
This allows fixing data from files, too. This simplifies troubleshooting
the fixup code.

Added fixup for timestamps of overlapping blocks.

srmdata.c
@@ -111,7 +111,7 @@ srm_marker_t srm_marker_clone( srm_marker_t marker )
 	tmp->first = marker->first;
 	tmp->last = marker->last;
 
-	if( NULL == (tmp->notes = strdup( marker->notes ) ))
+	if( marker->notes && NULL == (tmp->notes = strdup( marker->notes ) ))
 		goto clean1;
 
 	return tmp;
@@ -169,6 +169,197 @@ clean1:
 	free( data );
 	return NULL;
 }
+
+/*
+ * add chunk to data.
+ * chunk's timestamp is adjusted to fit recint
+ * if there's a gap, it's filled with averaged data
+ *
+ * on success 0 is returned
+ * returns -1 and sets errno on error
+ */
+int srm_data_add_fillp( srm_data_t data, srm_chunk_t chunk )
+{
+	srm_chunk_t last = data->chunks[data->cused-1];
+	srm_time_t lnext = last->time + data->recint;
+	unsigned miss;
+	unsigned i;
+
+	if( chunk->time < lnext ){
+		DPRINTF( "srm_data_add_fillp: data is overlapping, can't add" );
+		errno = EINVAL;
+		return -1;
+	}
+
+	miss = (chunk->time - lnext) / data->recint;
+
+	/* ... adjust current time to fit n*recint */
+	lnext += data->recint * miss;
+	if( chunk->time > lnext ){
+		errno = EOVERFLOW;
+		return -1;
+	}
+
+	if( chunk->time != lnext ){
+		DPRINTF("srm_data_add_fill: adjusting gap %.1f -> %.1f",
+			(double)chunk->time / 10,
+			(double)lnext / 10);
+		chunk->time = lnext;
+	}
+
+	if( ! miss )
+		return 0;
+
+	DPRINTF( "srm_data_add_fill: synthesizing %d chunks @%.1f",
+		miss, (double)lnext / 10 );
+
+	/* ... adjust marker indices */
+	for( i=0; i < data->mused; ++i ){
+		srm_marker_t mark = data->marker[i];
+
+		if( mark->first >= data->cused )
+			mark->first += miss;
+
+		if( mark->last >= data->cused )
+			mark->last += miss;
+	}
+
+	/* ... insert averaged data */
+	for( i = 1; i <= miss; ++i ){
+		srm_chunk_t fill;
+		double part = (double)i / (miss +1 );
+
+		if( NULL == (fill = srm_chunk_new() ))
+			return -1;
+
+		fill->time = last->time + (i * data->recint);
+		fill->temp = part * (chunk->temp
+			- last->temp) + last->temp;
+		fill->pwr = part * (int)( chunk->pwr - last->pwr )
+			+ last->pwr + 0.5;
+		fill->speed = part * (chunk->speed
+			- last->speed) + last->speed;
+		fill->cad = part * (int)( chunk->cad - last->cad )
+			+ last->cad + 0.5;
+		fill->hr = part * (int)( chunk->hr
+			- last->hr ) + last->hr;
+		fill->ele = part * (chunk->ele - last->ele)
+			+ last->ele + 0.5;
+
+		if( 0 > srm_data_add_chunkp( data, fill ) ){
+			srm_chunk_free(fill);
+			return -1;
+		}
+	}
+
+	if( 0 > srm_data_add_chunkp( data, chunk ) )
+		return -1;
+
+	return 0;
+}
+
+
+/*
+ * fix small time leaps at block boundaries
+ * fix timestamps of overlapping chunks
+ * fill small gaps with averaged data
+ * fixed data is copied to a new srm_data_t handle
+ *
+ * returns pointer to newly allocated srm_data
+ * returns NULL on failure
+ */
+srm_data_t srm_data_fixup( srm_data_t data )
+{
+	srm_data_t fixed;
+	srm_time_t delta = 0;
+	unsigned c, m;
+
+	if( data->cused < 1 )
+		return NULL;
+
+	if( NULL == (fixed = srm_data_new() ))
+		return NULL;
+
+	/* copy global data */
+	fixed->recint = data->recint;
+	fixed->slope = data->slope;
+	fixed->zeropos = data->zeropos;
+	if( data->notes && NULL == (fixed->notes = strdup( data->notes ) ))
+		goto clean1;
+
+	/* copy marker */
+	for( m=0; m < data->mused; ++m ){
+		srm_marker_t mark;
+
+		if( NULL == ( mark = srm_marker_clone( data->marker[m]) ) )
+			goto clean1;
+
+		if( 0 > srm_data_add_markerp( fixed, mark ) )
+			goto clean1;
+	}
+
+	/* copy chunks + fix smaller gaps/overlaps */
+
+	if( 0 > srm_data_add_chunk( fixed, data->chunks[0] ) )
+		goto clean1;
+
+	for( c=1; c < data->cused; ++c ){
+		srm_chunk_t last = fixed->chunks[fixed->cused-1];
+		srm_time_t lnext = last->time + fixed->recint;
+		srm_chunk_t this;
+
+		if( NULL == ( this = srm_chunk_clone( data->chunks[c] )))
+			goto clean1;
+
+		/* overlapping < 1sec, adjust this time */
+		if( this->time < lnext && lnext - last->time < 10 ){
+			DPRINTF("srm_data_fixup: adjusting overlap %.1f -> %.1f",
+				(double)this->time / 10,
+				(double)lnext / 10);
+			this->time = lnext;
+
+			if( 0 > srm_data_add_chunkp( fixed, this ) )
+				goto clean1;
+
+		/* gap <= 2sec ... */
+		} else if ( lnext < this->time
+			&& this->time - lnext <= 20) {
+
+			if( 0 > srm_data_add_fillp( fixed, this ))
+				goto clean1;
+
+		/* nothing to fix (yet) */
+		} else {
+			if( 0 > srm_data_add_chunkp( fixed, this ) )
+				goto clean1;
+
+		}
+
+	}
+
+	/* fix "severe" overlaping */
+	for( c = fixed->cused -1; c > 0; --c ){
+		srm_chunk_t this = fixed->chunks[c-1];
+		srm_chunk_t next = fixed->chunks[c];
+		srm_time_t nprev = next->time - fixed->recint - delta;
+
+		if( nprev < this->time ){
+			delta += this->time - nprev;
+			DPRINTF( "srm_data_fixup overlaping blocks @%d, "
+				"new delta: %.1f",
+				c,
+				(double)delta/10 );
+		}
+		this->time -= delta;
+	}
+
+	return fixed;
+
+clean1:
+	srm_data_free( fixed );
+	return NULL;
+}
+
 
 /*
  * add chunk to end of data's chunk list. Extends list when necessary.

srmcmd.c
@@ -227,6 +227,16 @@ int main( int argc, char **argv )
 			return 1;
 		}
 
+		if( opt_fixup ){
+			srm_data_t fixed;
+
+			if( NULL == ( fixed = srm_data_fixup( srmdata ) ) )
+				return -1;
+
+			srm_data_free( srmdata );
+			srmdata = fixed;
+		}
+
 		if( opt_name ){
 			if( ! srmdata->mused ){
 				fprintf( stderr, "no data available\n" );

srmio.h
@@ -81,6 +81,7 @@ struct _srm_data_t {
 typedef struct _srm_data_t *srm_data_t;
 
 srm_data_t srm_data_new( void );
+srm_data_t srm_data_fixup( srm_data_t data );
 
 int srm_data_add_chunkp( srm_data_t data, srm_chunk_t chunk );
 int srm_data_add_chunk( srm_data_t data, srm_chunk_t chunk );
@@ -153,7 +154,6 @@ int srmpc_set_recint( srmpc_conn_t conn, srm_time_t recint );
 struct _srmpc_get_chunk_t {
 	/* whole downlad */
 	srmpc_conn_t		conn;
-	int			fixup;
 	struct tm		pctime;
 	unsigned		blocks;
 
@@ -175,7 +175,7 @@ struct _srmpc_get_chunk_t {
 typedef struct _srmpc_get_chunk_t *srmpc_get_chunk_t;
 
 typedef int (*srmpc_chunk_callback_t)( srmpc_get_chunk_t gh );
-int srmpc_get_chunks( srmpc_conn_t conn, int getall, int fixup,
+int srmpc_get_chunks( srmpc_conn_t conn, int getall,
 	srmpc_chunk_callback_t cfunc, void *cbdata );
 
 int srmpc_clear_chunks( srmpc_conn_t conn );

srmpc.c
@@ -297,7 +297,7 @@ static int _srmpc_init_all( srmpc_conn_t conn )
  *  fname: path to serial device
  *  force: ignore whitelist (when != 0)
  *  lfunc: function to invoke for verbose status reporting
- * 
+ *
  * returns newly allocated connection handle.
  *
  * on error errno is set and returns NULL
@@ -329,7 +329,7 @@ srmpc_conn_t srmpc_open( const char *fname, int force,
 
 	/* set serial parameter and get PCV version */
 	/* TODO: allow user to specify baudrate+parity */
-	if( 0 > ( pcv = _srmpc_init_all( conn ))) 
+	if( 0 > ( pcv = _srmpc_init_all( conn )))
 		goto clean3;
 
 
@@ -353,7 +353,7 @@ srmpc_conn_t srmpc_open( const char *fname, int force,
 	}
 
 	return conn;
-	
+
 clean3:
 	close(conn->fd);
 
@@ -1194,7 +1194,6 @@ static int _srmpc_parse_block( srmpc_get_chunk_t gh,
 	srmpc_chunk_callback_t	cbfunc )
 {
 	struct tm btm;
-	srm_time_t bstart, lnext;
 	int ret;
 
 	DUMPHEX( "_srmpc_parse_block", buf, 64 );
@@ -1215,55 +1214,7 @@ static int _srmpc_parse_block( srmpc_get_chunk_t gh,
 	if( 0 > ret )
 		return -1;
 
-	bstart = (srm_time_t)ret *10;
-
-	/* adjust block timestamp based on previous one */
-	/* TODO: postprocess data for fixup *after* download, move to
-	 * srmdata.c */
-	/* TODO: fixup overlapping block timestamps */
-	lnext = gh->bstart + 11 * gh->recint;
-	if( gh->bstart > lnext ){
-		errno = EOVERFLOW;
-		return -1;
-	}
-
-	if( gh->fixup && gh->recint && lnext 
-		&& (lnext != bstart) ){
-
-		/* overlapping < 1sec */
-		if( bstart < lnext && lnext - bstart < 10 ){
-			gh->bstart = lnext;
-
-		/* gap < 2sec */
-		} else if ( bstart > lnext && bstart - lnext <= 20 ){
-			unsigned cspans = (bstart - lnext) / gh->recint;
-			gh->bstart = lnext + gh->recint * cspans;
-			if( gh->bstart < lnext ){
-				errno = EOVERFLOW;
-				return -1;
-			}
-
-		/* bigger difference */
-		} else {
-			gh->bstart = bstart;
-		}
-
-#ifdef DEBUG
-		if( gh->bstart != bstart ){
-		DPRINTF( "_srmpc_parse_block adj. timestamp %.1f > (%.1f) > %.1f",
-			(double)bstart/10,
-			(double)lnext/10,
-			(double)gh->bstart/10);
-		}
-#endif
-
-
-	} else {
-		gh->bstart = bstart;
-
-	}
-
-
+	gh->bstart = (srm_time_t)ret *10;
 	gh->dist = ( (buf[5] << 16 )
 		| ( buf[6] << 8 )
 		| buf[7] )
@@ -1328,6 +1279,9 @@ static int _srmpc_parse_block( srmpc_get_chunk_t gh,
 	return 0;
 }
 
+
+/* TODO: convert to non-callback interface */
+
 /*
  * get all blocks/chunks off the SRM, parse it and pass the decoded chunks
  * to the callback function.
@@ -1335,7 +1289,6 @@ static int _srmpc_parse_block( srmpc_get_chunk_t gh,
  * parameter:
  *  conn: connection handle
  *  deleted: instruct PCV to send deleted data, too (when != 0)
- *  fixup: postprocess data (fix timestamps, fill micro-gaps, ...) (when != 0)
  *  cbfunc: callback to process each retrieved data chunk
  *  cbdata: passed to cbfunc
  *
@@ -1344,7 +1297,6 @@ static int _srmpc_parse_block( srmpc_get_chunk_t gh,
 int srmpc_get_chunks( 
 	srmpc_conn_t conn, 
 	int deleted,
-	int fixup,
 	srmpc_chunk_callback_t cbfunc, 
 	void *cbdata )
 {
@@ -1365,7 +1317,6 @@ int srmpc_get_chunks(
 
 	memset(&gh, 0, sizeof( struct _srmpc_get_chunk_t ));
 	gh.conn = conn;
-	gh.fixup = fixup;
 	gh.cbdata = cbdata;
 
 	if( 0 > srmpc_get_time( conn, &gh.pctime ))
@@ -1533,64 +1484,6 @@ int srmpc_clear_chunks( srmpc_conn_t conn )
  ************************************************************/
 
 
-/*
- * synthesize some chunks with average data to fill small gaps at block
- * boundaries.
- */
-static int _srmpc_chunk_data_gapfill( srmpc_get_chunk_t gh )
-{
-	struct _srmpc_get_data_t *gdat = gh->cbdata;
-	srm_chunk_t nck = &gh->chunk;
-	srm_chunk_t lck;
-	srm_time_t delta;
-	unsigned fillnum, num;
-
-	if( gdat->data->cused < 1 )
-		return 0;
-
-	lck = gdat->data->chunks[gdat->data->cused-1];
-	delta = nck->time - lck->time;
-
-	/* no gap */
-	if( delta <= gdat->data->recint )
-		return 0;
-	
-	delta -= gdat->data->recint;
-	if( delta > 20 )
-		return 0; /* too large */
-
-	fillnum = delta / gdat->data->recint;
-	_srm_log( gh->conn, "inserting %u chunks to fill micro-gap at chunk#%u",
-		fillnum,
-		gdat->data->cused );
-
-	for( num = 1; num <= fillnum; ++num ){
-		srm_chunk_t fill;
-		double part = (double)num / (fillnum +1 );
-
-		if( NULL == (fill = srm_chunk_new() ))
-			return -1;
-
-		fill->time = lck->time + (num * gdat->data->recint);
-		fill->temp = part * (nck->temp - lck->temp) + lck->temp;
-		fill->pwr = part * (int)( nck->pwr - lck->pwr ) 
-			+ lck->pwr + 0.5;
-		fill->speed = part * (nck->speed - lck->speed) + lck->speed;
-		fill->cad = part * (int)( nck->cad - lck->cad ) 
-			+ lck->cad + 0.5;
-		fill->hr = part * (int)( nck->hr - lck->hr ) + lck->hr;
-		fill->ele = part * (nck->ele - lck->ele) 
-			+ lck->ele + 0.5;
-
-		if( 0 > srm_data_add_chunkp( gdat->data, fill ) ){
-			srm_chunk_free(fill);
-			return -1;
-		}
-	}
-
-	return 0;
-}
-
 static int _srmpc_chunk_data_cb( srmpc_get_chunk_t gh )
 {
 	struct _srmpc_get_data_t *gdat = (struct _srmpc_get_data_t *)gh->cbdata;
@@ -1599,14 +1492,6 @@ static int _srmpc_chunk_data_cb( srmpc_get_chunk_t gh )
 	/* TODO: start new file on recint change */
 	if( ! gdat->data->recint )
 		gdat->data->recint = gh->recint;
-
-	/* TODO: postprocess data for fixup *after* download, move to
-	 * srmdata.c */
-
-	/* fill small gaps (<= 2sec) at block boundaries with averaged data? */
-	if( gh->fixup && gh->chunknum == 0 )
-		if( 0 > _srmpc_chunk_data_gapfill( gh ))
-			return -1;
 
 	if( 0 > srm_data_add_chunk( gdat->data, &gh->chunk ) )
 		return -1;
@@ -1671,7 +1556,7 @@ srm_data_t srmpc_get_data( srmpc_conn_t conn, int deleted, int fixup )
 	if( 0 > srm_data_add_markerp( gdat.data, mk ))
 		goto clean2;
 
-	if( 0 > srmpc_get_chunks(conn, deleted, fixup, _srmpc_chunk_data_cb, &gdat ) )
+	if( 0 > srmpc_get_chunks(conn, deleted, _srmpc_chunk_data_cb, &gdat ) )
 		goto clean2;
 
 	gdat.data->marker[0]->last = gdat.data->cused-1;
@@ -1679,6 +1564,16 @@ srm_data_t srmpc_get_data( srmpc_conn_t conn, int deleted, int fixup )
 	if( gdat.mfirst >= 0 )
 		srm_data_add_marker( gdat.data, gdat.mfirst,
 			gdat.data->cused -1 );
+
+	if( fixup ){
+		srm_data_t fixed;
+
+		if( NULL == ( fixed = srm_data_fixup( gdat.data ) ) )
+			goto clean2;
+
+		srm_data_free( gdat.data );
+		gdat.data = fixed;
+	}
 
 	return gdat.data;
 

commit 1a7f1ac64bc3f6befe6a5a1c81c0fe95522e1851

corrected data fixup for recint > 1sec
data fixup failed to move chunks to fill gaps < recint. This bug was
introduced in 2da86c0ecce19625e8808e929d8b422aa2cb4871.

srmdata.c
@@ -192,18 +192,13 @@ int srm_data_add_fillp( srm_data_t data, srm_chunk_t chunk )
 		return -1;
 	}
 
-	miss = (chunk->time - lnext) / recint;
+	miss = 0.4 + (chunk->time - lnext) / recint;
+	lnext += recint * miss;
+
 
 	/* ... adjust current time to fit n*recint */
-	lnext += recint * miss;
-	if( chunk->time > lnext ){
-		ERRMSG( "srm_data_add_fillp: cannot fill gaps < recint" );
-		errno = EOVERFLOW;
-		return -1;
-	}
-
 	if( chunk->time != lnext ){
-		ERRMSG("srm_data_add_fill: adjusting gap %.1f -> %.1f",
+		DPRINTF("srm_data_add_fill: adjusting gap %.1f -> %.1f",
 			(double)chunk->time / 10,
 			(double)lnext / 10);
 		chunk->time = lnext;
@@ -319,7 +314,9 @@ srm_data_t srm_data_fixup( srm_data_t data )
 		lnext = last->time + recint;
 
 		/* overlapping < 1sec, adjust this time */
-		if( this->time < lnext && lnext - last->time < 10 ){
+		if( this->time < lnext
+			&& lnext - last->time < 10 ){
+
 			DPRINTF("srm_data_fixup: adjusting overlap %.1f -> %.1f",
 				(double)this->time / 10,
 				(double)lnext / 10);
@@ -328,9 +325,9 @@ srm_data_t srm_data_fixup( srm_data_t data )
 			if( 0 > srm_data_add_chunkp( fixed, this ) )
 				goto clean1;
 
-		/* gap <= 2sec ... */
-		} else if ( lnext < this->time
-			&& this->time - lnext <= 20) {
+		/* small gap > recint ... fill/shift  */
+		} else if( lnext < this->time
+			&& this->time - lnext <= 2*recint ){
 
 			if( 0 > srm_data_add_fillp( fixed, this ))
 				goto clean1;